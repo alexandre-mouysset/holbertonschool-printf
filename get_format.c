620f7d2043ddafa6cf1cfb606103322236b67c7c
tomvieilledent <tomvieilledent@gmail.com>
2025-11-28 11:22:22 +0100

    Add printf_x and printf_X functions for hexadecimal format printing

--- get_format.c
+++ get_format.c
@@ -16,6 +16,8 @@ int (*get_format(char letter))(va_list)
 		{'d', printf_d},
 		{'u', printf_u},
 		{'o', printf_o},
+		{'x', printf_x},
+		{'X', printf_X},
 		{'b', printf_b},
 		{'%', printf_37},
 		{'\0', NULL}};
